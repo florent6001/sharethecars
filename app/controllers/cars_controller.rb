9aba2805d68469b04551593e705433869fbad74d
Florent Soler <florentdu13200@hotmail.fr>
2024-03-19 14:59:08 +0100

    car show car list

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,17 +1,11 @@
 class CarsController < ApplicationController
-  before_action :set_company, only: [:index]
+  before_action :set_company
 
   def index
-    @cars = @company.cars.includes(:company)
+    @cars = @user_company.cars
   end
 
   def show
     @car = Car.find(params[:id])
-  end
-
-  private
-
-  def set_company
-    @company = Company.find(1)
   end
 end
