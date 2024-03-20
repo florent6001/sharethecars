61b55eecbbdf72e35de786ed39fe91f143e67b1e
Florent Vandroy <florentvandroy@gmail.com>
2024-03-20 14:59:46 +0100

    Add dashboard cards, feedback form, and feedbacks to cars

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :company
+  has_many :reservations
+  has_many :feedbacks, through: :reservations
   has_one_attached :photo
   validates :model, :color, :plate_number, presence: true
   validates :company, presence: true
