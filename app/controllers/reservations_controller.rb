b9db2df26541aa34849196aa87508bac73b7b7b8
Daniel Guasco <daniele.scultura@libero.it>
2024-03-21 15:05:06 +0100

    create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,27 @@
 class ReservationsController < ApplicationController
+  before_action :set_car, only: [:create]
+
   def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    @reservation.car = @car
+    @reservation.save!
+    redirect_to dashboard_path, notice: "Votre réservation a bien été prise en compte."
   end
 
   def destroy
     @reservation = Reservation.find(params[:id])
     @reservation.destroy
     redirect_to dashboard_path
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
   end
 end
