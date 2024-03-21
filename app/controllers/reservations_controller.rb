96c87cefc8853eff575901ebc271e8947a55ea7e
Florent Soler <florentdu13200@hotmail.fr>
2024-03-21 14:18:13 +0100

    Ajouter la fonctionnalité de suppression de réservation et de confirmation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,10 @@
 class ReservationsController < ApplicationController
   def create
+  end
 
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to dashboard_path
   end
 end
