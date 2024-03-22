a7b75d1a7f7a6edaf69d7e7f567f884e4c2f27c4
Florent Soler <florentdu13200@hotmail.fr>
2024-03-22 09:03:47 +0100

    J'ai rajouter un fichier de migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_19_133200) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_22_015408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_19_133200) do
     t.date "end_date"
     t.bigint "car_id", null: false
     t.float "kilometers"
-    t.boolean "done"
+    t.boolean "done", default: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
