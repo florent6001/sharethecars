5075f77fecd8f0b0aae3cb587c3e8648e9c3acb3
Daniel Guasco <daniele.scultura@libero.it>
2024-03-25 17:46:47 +0100

    Add team page and bio to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_015408) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_25_140409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_22_015408) do
     t.string "phone_number"
     t.boolean "owner"
     t.bigint "company_id"
+    t.text "bio"
     t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
